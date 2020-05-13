0be6be2312dbc922a10ca707c24acac3ce64fbdd
rrrochaf <ricardorrf@gmail.com>
2020-05-13 17:57:57 -0300

    adding POJOs

---
+++ spring-pet-clinic/src/main/java/guru/springframework/springpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package guru.springframework.springpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ spring-pet-clinic/src/main/java/guru/springframework/springpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package guru.springframework.springpetclinic.model;
+
+public class Vet extends Person{
+	
+	
+
+}
