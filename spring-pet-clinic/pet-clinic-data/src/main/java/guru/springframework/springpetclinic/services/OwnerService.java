2a26b57728f03402ff334d715fb85504d41492c4
rrrochaf <ricardorrf@gmail.com>
2020-05-15 10:15:35 -0300

    Refactor Service Interface to Common Base Interface

--- spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/OwnerService.java
+++ spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/OwnerService.java
@@ -1,17 +1,9 @@
 package guru.springframework.springpetclinic.services;
 
-import java.util.Set;
-
 import guru.springframework.springpetclinic.model.Owner;
 
-public interface OwnerService {
+
+public interface OwnerService extends CrudService<Owner, Long>{
 	
 	Owner findByLastName(String lastName);
-	
-	Owner findById(Long id);
-	
-	Owner save(Owner owner);
-	
-	Set<Owner> findAll();
-
 }
--- spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/VetService.java
+++ spring-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/VetService.java
@@ -1,14 +1,7 @@
 package guru.springframework.springpetclinic.services;
 
-import java.util.Set;
-
 import guru.springframework.springpetclinic.model.Vet;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
-	Vet findById(Long id);
-
-	Vet save(Vet vet);
-
-	Set<Vet> findAll();
 }
